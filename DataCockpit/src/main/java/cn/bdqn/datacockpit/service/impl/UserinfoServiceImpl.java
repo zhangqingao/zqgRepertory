6f1087b7b53aa58c94c91dfe8baf057564084f47
YuanTian <YuanTian@DESKTOP-H7QI7SH.lan>
2017-08-24 22:56:49 +0800

    更新UserInfoserviceimpl

--- DataCockpit/src/main/java/cn/bdqn/datacockpit/service/impl/UserinfoServiceImpl.java
+++ DataCockpit/src/main/java/cn/bdqn/datacockpit/service/impl/UserinfoServiceImpl.java
@@ -14,7 +14,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import cn.bdqn.datacockpit.mapper.Userinfo;
+import cn.bdqn.datacockpit.entity.Userinfo;
 import cn.bdqn.datacockpit.mapper.UserinfoMapper;
 import cn.bdqn.datacockpit.service.UserinfoService;
 
