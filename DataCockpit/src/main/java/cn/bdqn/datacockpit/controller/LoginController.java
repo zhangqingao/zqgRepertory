281319868944d06399cccb953ea368604e16c2ff
chenxitong <2718472803@qq.com>
2017-12-07 19:00:19 +0800

    hhhhhh

--- DataCockpit/src/main/java/cn/bdqn/datacockpit/controller/LoginController.java
+++ DataCockpit/src/main/java/cn/bdqn/datacockpit/controller/LoginController.java
@@ -74,7 +74,7 @@ public class LoginController {
             String verifyCode = VerifyCodeUtils.generateVerifyCode(4);
             // 存入会话session
             HttpSession session = request.getSession(true);
-            session.setAttribute("code", verifyCode.toLowerCase());
+            session.setAttribute("code", verifyCode.toLowerCase());// 
             // 生成图片
             int w = 146, h = 33;
             VerifyCodeUtils.outputImage(w, h, response.getOutputStream(), verifyCode);
@@ -115,7 +115,7 @@ public class LoginController {
         // 根据账号判断该用户属于公司还是管理员
         List<Info> infoList = infoService.selectAllInfo();
         Date time = new Date();
-        Date ti1 = new Date(time.getTime() - 2 * 24 * 60 * 60 * 1000);
+        Date ti1 = new Date(time.getTime() - 7 * 24 * 60 * 60 * 1000);
         for (Info info : infoList) {
             Date date = info.getPublishDate();
             Map<String, Object> map = new HashMap<String, Object>();
